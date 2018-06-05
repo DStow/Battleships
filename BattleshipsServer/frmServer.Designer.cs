c0fc51e333760464c9ee9e427a7df35ca2b29817
Dan Stow <Dan_Stow@live.co.uk>
2018-06-05 21:55:28 +0100

    Sorted out GUI

--- BattleshipsServer/frmServer.Designer.cs
+++ BattleshipsServer/frmServer.Designer.cs
@@ -28,21 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnStartServer = new System.Windows.Forms.Button();
+            this.btnStopServer = new System.Windows.Forms.Button();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtServerLog = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnStartServer
+            // 
+            this.btnStartServer.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnStartServer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStartServer.Location = new System.Drawing.Point(118, 12);
+            this.btnStartServer.Name = "btnStartServer";
+            this.btnStartServer.Size = new System.Drawing.Size(116, 74);
+            this.btnStartServer.TabIndex = 0;
+            this.btnStartServer.Text = "Start Server";
+            this.btnStartServer.UseVisualStyleBackColor = true;
+            this.btnStartServer.Click += new System.EventHandler(this.btnStartServer_Click);
+            // 
+            // btnStopServer
+            // 
+            this.btnStopServer.Enabled = false;
+            this.btnStopServer.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnStopServer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStopServer.Location = new System.Drawing.Point(240, 12);
+            this.btnStopServer.Name = "btnStopServer";
+            this.btnStopServer.Size = new System.Drawing.Size(116, 74);
+            this.btnStopServer.TabIndex = 1;
+            this.btnStopServer.Text = "Stop Server";
+            this.btnStopServer.UseVisualStyleBackColor = true;
+            this.btnStopServer.Click += new System.EventHandler(this.btnStopServer_Click);
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(12, 62);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(100, 24);
+            this.txtPort.TabIndex = 2;
+            this.txtPort.Text = "1234";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Port:";
+            // 
+            // txtServerLog
+            // 
+            this.txtServerLog.BackColor = System.Drawing.Color.Navy;
+            this.txtServerLog.ForeColor = System.Drawing.Color.White;
+            this.txtServerLog.Location = new System.Drawing.Point(12, 131);
+            this.txtServerLog.Multiline = true;
+            this.txtServerLog.Name = "txtServerLog";
+            this.txtServerLog.ReadOnly = true;
+            this.txtServerLog.Size = new System.Drawing.Size(344, 275);
+            this.txtServerLog.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Server Log:";
             // 
             // frmServer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 601);
+            this.ClientSize = new System.Drawing.Size(366, 413);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtServerLog);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.btnStopServer);
+            this.Controls.Add(this.btnStartServer);
             this.Font = new System.Drawing.Font("Calibri", 10F);
             this.Name = "frmServer";
             this.Text = "Battleships Server";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmServer_FormClosing);
+            this.Load += new System.EventHandler(this.frmServer_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStartServer;
+        private System.Windows.Forms.Button btnStopServer;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtServerLog;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- BattleshipsServer/frmServer.cs
+++ BattleshipsServer/frmServer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,89 @@ namespace BattleshipsServer
 {
     public partial class frmServer : Form
     {
+        private TCPServerLib.Server _server;
+        private string _logDirectory = "";
+
         public frmServer()
         {
             InitializeComponent();
+        }
+
+        private void frmServer_Load(object sender, EventArgs e)
+        {
+            // Check if log folder exists
+            _logDirectory = Application.StartupPath + "\\Log";
+
+            if (Directory.Exists(_logDirectory) == false)
+            {
+                Directory.CreateDirectory(_logDirectory);
+            }
+        }
+
+        private void btnStartServer_Click(object sender, EventArgs e)
+        {
+            // Check if port is a valid number?
+            string sPort = txtPort.Text;
+
+            int port = 0;
+            if (int.TryParse(sPort, out port) == false)
+            {
+                MessageBox.Show("Please enter a valid number for the port!");
+                return;
+            }
+            else if (port < 1000 || port > 9999)
+            {
+                MessageBox.Show("Please enter a port between 1000 and 9999!");
+                return;
+            }
+            else
+            {
+                _server = new TCPServerLib.Server(port, ProcessIncomingMessage);
+                _server.StartServer();
+
+                WriteToLog("Server started on port: " + port);
+
+                btnStopServer.Enabled = true;
+                btnStartServer.Enabled = false;
+            }
+        }
+
+        private void btnStopServer_Click(object sender, EventArgs e)
+        {
+            _server.CloseServer();
+
+            WriteToLog("Server stopped");
+
+            btnStopServer.Enabled = false;
+            btnStartServer.Enabled = true;
+        }
+
+        private void WriteToLog(string message)
+        {
+            // Write to textbox and to log file
+            string lowKeyMessage = DateTime.Now.ToString("HH:mm:ss.fff") + " - " + message + "\n\r";
+            string fullMessage = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + " - " + message;
+
+            // Write to GUI
+            txtServerLog.Text = lowKeyMessage + "\r\n" + txtServerLog.Text;
+
+            // Write to text file
+            string logFile = _logDirectory + "\\Log " + DateTime.Now.ToString("yyyyMMdd") + ".txt";
+            StreamWriter sw = new StreamWriter(logFile, true);
+            sw.WriteLine(fullMessage);
+            sw.Close();
+        }
+
+        private void frmServer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _server.CloseServer();
+        }
+
+
+        // Deals with any message that is recieved by the server while it's running
+        private string ProcessIncomingMessage(string message)
+        {
+            return "Recieved";
         }
     }
 }
