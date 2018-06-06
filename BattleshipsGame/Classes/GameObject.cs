b7d58ee9e4f844f29bf1d54d23a8a6b94d605040
Dan Stow <Dan_Stow@live.co.uk>
2018-06-06 22:27:03 +0100

    Add a base game object to inherit from

---
+++ BattleshipsGame/Classes/GameObject.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace BattleshipsGame.Classes
+{
+    public abstract class GameObject
+    {
+        public Vector2 Position { get; set; }
+        public Vector2 Size { get; set; }
+
+        public MonoGameUtilityLib.RectangleF AreaF
+        {
+            get
+            {
+                return new MonoGameUtilityLib.RectangleF(Position.X, Position.Y, Size.X, Size.Y);
+            }
+        }
+
+        public virtual void LoadContent(ContentManager content)
+        {
+
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
