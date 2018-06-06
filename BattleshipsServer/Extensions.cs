512278706437f09e148018a494c5eda5ec499a3c
Dan Stow <Dan_Stow@live.co.uk>
2018-06-06 21:13:24 +0100

    Add extension class for checking if a string array has a value or returning null

---
+++ BattleshipsServer/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipsServer.Extensions
+{
+    public static class Extensions
+    {
+        public static string GetPiecesOrNull(this string[] pieces, int index)
+        {
+            if (index >= pieces.Count())
+            {
+                return null;
+            }
+            else
+            {
+                return pieces[index];
+            }
+        }
+    }
+}
