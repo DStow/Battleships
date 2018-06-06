2d81b82e75ff67a588379ca282fd046e403732b9
Dan Stow <Dan_Stow@live.co.uk>
2018-06-06 22:20:25 +0100

    Add empty BattleshipsGame MonoGame project

---
+++ BattleshipsGame/BattleshipsGame.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace BattleshipsGame
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class BattleshipsGame : Game
+    {
+        GraphicsDeviceManager _graphics;
+        SpriteBatch _spriteBatch;
+
+        public BattleshipsGame()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 640;
+        }
+
+        protected override void Initialize()
+        {
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+     
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+    
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
